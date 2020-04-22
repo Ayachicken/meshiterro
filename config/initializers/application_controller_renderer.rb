51598d6cdb6c1b775808554a900aee452900e2ba
Ayachicken <digimonmon.a@gmail.com>
2020-04-22 19:12:26 +0900

    [finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'aa718f29067e661c31991487750aee6ffdd031c17e5c2967fd2cf0b9f3a82abc23fc390129eba2c9b8db8e5996697e2a7cb6b95c1f67e7dc1184fb699d45b2da'
